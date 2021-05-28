ae2a0cbc75127bda05a504cd219b18aa55b68196
Vighnesh Kulkarni <vighnesh192000@gmail.com>
2021-05-28 19:58:09 +0530

    Actions and Reducers created for ProjectDetails page

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -7,3 +7,14 @@ export const setProjects = (projects, query) => {
         }
     }
 }
+
+export const setProjectDetails = (projects, queryType, projectDetails) => {
+    return {
+        type: 'SET_PROJECT_DETAILS',
+        payload: {
+            projects,
+            queryType,
+            projectDetails
+        }
+    }
+}
--- src/reducers/projectReducers.js
+++ src/reducers/projectReducers.js
@@ -1,18 +1,22 @@
 const initialState = {
     projects: undefined,
-    queryType: 'popular'
+    queryType: 'popular',
+    projectDetails: undefined
 };
 
 const projectReducer = (state = initialState, action) => {
+    const { projects, queryType, projectDetails } = action.payload;
     switch (action.type) {
         case 'SET':
-            state = {
-                projects: action.payload.projects,
-                queryType: action.payload.queryType
-            };
+            state = { projects, queryType };
             console.log('PROJECT STATE' ,state);
             return state;
-    
+        
+        case 'SET_PROJECT_DETAILS':
+            state = { projects, queryType, projectDetails };
+            console.log('PROJECT_DETAILS_REDUCER:-', state);
+            return state;
+
         default:
             return state;
     }
